9e464feedbc7d6d056d42ee50db3caf29f1a727c
Mary-Maina <richellwanjiru@gmail.com>
2024-03-18 02:35:11 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        sentence[0] == None
+    else:
+        length = len(sentence)
+        s = sentence[0]
+
+        return (length, s)
