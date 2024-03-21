4f27f62e13316cc6fd9e8fdff475a97b06ea16e0
Mary-Maina <richellwanjiru@gmail.com>
2024-03-20 19:47:20 -0700

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    key_delete = []
+    for k,v in a_ditionary.items():
+        if v == value:
+            key_delete.append(k)
+    for key in key_delete:
+        del a_dictionary[key]
+
+    return a_dictionary
