8d288866f406eccf99e372821d26cc2e017b758f
Mary-Maina <richellwanjiru@gmail.com>
2024-03-20 19:51:58 -0700

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     key_delete = []
-    for k,v in a_ditionary.items():
+    for k, v in a_ditionary.items():
         if v == value:
             key_delete.append(k)
     for key in key_delete:
