54658087b78f912292be5331cba770a210cf389d
Mary-Maina <richellwanjiru@gmail.com>
2024-04-17 15:02:55 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""returns the attributes and methods of an object"""
+
+
 def lookup(obj):
-    """returns the attributes and methods of an object"""
+    """ returns att and methods
+    arg:
+    obj: object
+    returns:
+    List
+    """
     return dir(obj)
