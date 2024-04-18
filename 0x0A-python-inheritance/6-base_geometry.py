a46f71e345e585437064ccf71dfb8b19b5bdc5fc
Mary-Maina <richellwanjiru@gmail.com>
2024-04-18 01:45:47 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Inputs method area in BaseGeometry"""
+
+
+class BaseGeometry():
+    """
+    Looks at the method area
+    Raises an Exception
+    """
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
