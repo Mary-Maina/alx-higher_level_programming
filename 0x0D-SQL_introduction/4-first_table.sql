3f94c743aa866a38b30369eb04501f58b6b26915
Mary-Maina <richellwanjiru@gmail.com>
2024-05-23 13:28:09 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creating tables in SQL
-CREATE table IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
+CREATE table IF NOT EXISTS first_table(id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Printing all descriptions of a table
+SHOW CREATE table first_table
