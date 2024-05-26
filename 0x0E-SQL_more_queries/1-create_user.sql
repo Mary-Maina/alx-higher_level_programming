b0679d7ceba79159787c8f24d64f453453fd1a5a
Mary-Maina <richellwanjiru@gmail.com>
2024-05-26 16:15:44 -0700

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- How to create a server for a user and give them permission
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
