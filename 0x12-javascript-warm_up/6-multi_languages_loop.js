e4526e6cdc864654853f475ccc283f62d27dfe62
Mary-Maina <richellwanjiru@gmail.com>
2024-06-19 16:55:26 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
+// printing strings in form of an  array
 
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of languages) {
+  console.log(line);
+}
